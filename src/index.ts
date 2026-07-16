85df0f5708e586f6369423bb9287c7419f454763
maverickchampi <maverick.champi@gmail.com>
2026-07-16 10:52:17 -0500

    [update] export icon name and declare fontssize default in icon component

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import "./components/icon/icomoon/style.css";
 
 export * from "./components/button";
 export * from "./components/checkbox";
+export { IconName } from "./components/icon/props";
 export * from "./components/input";
 export * from "./components/textarea";
 export * from "./components/tooltip";
