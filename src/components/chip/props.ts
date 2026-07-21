fdc024f422268b982ed4b4d421843a115334d5fe
maverickchampi <maverick.champi@gmail.com>
2026-07-21 15:02:12 -0500

    [fix] readonly correction in chip component

--- src/components/chip/props.ts
+++ src/components/chip/props.ts
@@ -9,25 +9,33 @@ interface BaseChipProps {
   variant?: ChipVariant;
 }
 
-type ReadonlyChipProps = {
+type ReadonlyExplicitProps = {
   readonly: true;
   href?: never;
   onClick?: never;
   disabled?: never;
 } & Omit<React.ComponentPropsWithoutRef<"span">, "children">;
 
+type ReadonlyDefaultProps = {
+  readonly?: never;
+  href?: never;
+  onClick?: never;
+  disabled?: never;
+} & Omit<React.ComponentPropsWithoutRef<"span">, "children">;
+
 type LinkChipProps = {
-  readonly?: false;
+  readonly: false;
   href: string;
   onClick?: React.MouseEventHandler<HTMLAnchorElement>;
   disabled?: never;
 } & Omit<React.ComponentPropsWithoutRef<"a">, "onClick" | "children">;
 
 type ButtonChipProps = {
-  readonly?: false;
+  readonly: false;
   href?: never;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
 } & Omit<React.ComponentPropsWithoutRef<"button">, "onClick" | "disabled" | "children">;
 
-export type ChipProps = BaseChipProps & (ReadonlyChipProps | LinkChipProps | ButtonChipProps);
+export type ChipProps = BaseChipProps & 
+  (ReadonlyExplicitProps | ReadonlyDefaultProps | LinkChipProps | ButtonChipProps);
