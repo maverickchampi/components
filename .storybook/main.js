01f67c31dedccadd427815868c4f2b23dbb0c35b
maverickchampi <maverick.champi@gmail.com>
2026-07-21 11:11:59 -0500

    [fix] add prefix in vars for styles

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,26 @@
 /** @type {import('@storybook/react-vite').StorybookConfig} */
+import remarkGfm from "remark-gfm";
+
 export default {
   stories: [
     "../src/stories/introduction.mdx",
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm]
+          }
+        }
+      }
+    }
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {}
