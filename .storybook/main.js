6b48f538c22f871c698a9f6d420b0c30293414e3
maverickchampi <maverick.champi@gmail.com>
2026-07-21 12:35:36 -0500

    [fix] update colors in scrollToTop component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,11 @@
 /** @type {import('@storybook/react-vite').StorybookConfig} */
-import remarkGfm from "remark-gfm";
-
 export default {
   stories: [
     "../src/stories/introduction.mdx",
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-docs",
-      options: {
-        mdxPluginOptions: {
-          mdxCompileOptions: {
-            remarkPlugins: [remarkGfm]
-          }
-        }
-      }
-    }
-  ],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/react-vite",
     options: {}
